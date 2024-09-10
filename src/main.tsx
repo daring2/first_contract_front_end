67e98068f0d44dabd7ef184be53f7fde3d5e3109
daring <danila.daring@gmail.com>
2024-09-10 18:34:51 +0300

    fix manifest url

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://daring2.github.com/first_contract_front_end/tonconnect-manifest.json';
+const manifestUrl = 'https://daring2.github.io/first_contract_front_end/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
