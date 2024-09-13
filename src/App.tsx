8b01cbea6512bff3fdfb5f64a98564e1b30e3960
daring <danila.daring@gmail.com>
2024-09-13 16:53:55 +0300

    implement webapp

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
   const {
@@ -14,6 +15,7 @@ function App() {
     owner_address,
   } = useMainContract();
   const { connected } = useTonConnect()
+  const showAlert = () => { WebApp.showAlert("Hey there!"); };
   return (
     <div>
       <div>
@@ -21,6 +23,8 @@ function App() {
       </div>
       <div>
         <div className='Card'>
+          <b>Platform</b>
+          <div className='Hint'>{WebApp.platform}</div>
           <b>Our contract Address</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Owner Address</b>
@@ -32,20 +36,16 @@ function App() {
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
         </div>
-        <div className='Card'>
-          {connected && (
-            <a onClick={() => { sendIncrement(); }}>
-              Increment by 3
-            </a>
-          )}
-        </div>
-        <div className='Card'>
-          {connected && (
-            <a onClick={() => { sendDeposit(); }}>
-              Deposit of 0.5 TON
-            </a>
-          )}
-        </div>
+        <a onClick={() => { showAlert(); }}>Show Alert</a>
+        <br/>
+        {connected && (
+          <a onClick={() => { sendIncrement(); }}>Increment by 3</a>
+        )}
+        <br/>
+        {connected && (
+          <a onClick={() => { sendDeposit(); }}>Deposit of 0.5 TON</a>
+        )}
+        <br/>
       </div>
     </div>
   );
